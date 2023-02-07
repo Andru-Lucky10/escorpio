429ef16b1c403a4904c549ad3c2559a1416c477d
Andrea <cabanaandrea10@gmail.com>
2023-02-07 08:54:38 -0300

    primer_carga

---
+++ scriptgames.js
@@ -0,0 +1,9 @@
+let imagen=document.querySelector(img);
+
+const inicioDeArrastre=()=>{
+    console.log("se inicio el arrastre")
+}
+
+imagen.addEventListener("dragend",()=>{
+console. log("se solto la imagen")
+})/imagenesjc/Rompe1.pgn
---
+++ scripthistori.js
@@ -0,0 +1,12 @@
+let video=document.querySelector(".videoUno")
+const iniciarRep=()=>{
+    console.dir(video)
+    document.querySelector("#duracion").innerText=video.duration
+    video.play()
+}
+
+const pausarRep=()=>{
+    video.pause()
+}
+let numero=281.100771
+console. log(numero.toFixed(2))
